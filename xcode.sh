0f1b89111f61f72dc30fc3e1045239f4f165ec9a
nullgemm <nullgemm@mailbox.org>
2020-11-01 20:42:04 +0100

    update script, add details on how to update

--- xcode.sh
+++ xcode.sh
@@ -22,7 +22,11 @@ mv osxcross-master osxcross
 echo "downloading SDK"
 wget -O tmp/fetch-macos.py https://raw.githubusercontent.com/foxlet/macOS-Simple-KVM/master/tools/FetchMacOS/fetch-macos.py
 sed -i 's/, keyword="BaseSystem"//g' tmp/fetch-macos.py
-python3 tmp/fetch-macos.py -o tmp -p 061-26566 #061-62736
+
+# id found by downloading the CustomerSeed software update catalog from the URL found in fetch-macos.py
+# (https://swscan.apple.com/content/catalogs/others/index-10.15customerseed-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog).
+# 061-26566 refers to "CLTools_macOS1015_SDK.smd"; it should be easy to find updates by searching for "SDK.smd".
+python3 tmp/fetch-macos.py -o tmp -c CustomerSeed -p 061-26566
 
 echo "extracting SDK"
 pbzx tmp/CLTools_macOS1015_SDK.pkg | cpio -i -D tmp
